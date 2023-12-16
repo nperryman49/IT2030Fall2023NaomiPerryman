470f03493de1be8f278f193a5df97e35c4a9db6d
nperryman49 <93299325+nperryman49@users.noreply.github.com>
2023-12-16 16:36:21 -0500

    Add files via upload

---
+++ Week 16/SearchData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+
+namespace NaomiRetroCloset.Data
+{
+    public class SearchData
+    {
+       
+            private const string SearchKey = "search";
+            private const string TypeKey = "searchtype";
+
+            private ITempDataDictionary tempData { get; set; }
+            public SearchData(ITempDataDictionary temp) =>
+
+                tempData = temp;
+
+        public string SearchTerm
+        {
+            get => tempData.Peek(SearchKey)?.ToString();
+            set => tempData[SearchKey] = value;
+        }
+
+        public bool HasSearchTerm => !string.IsNullOrEmpty(SearchTerm);
+        //public bool IsProduct => Type.FilterName("product");
+
+        public void Clear()
+        {
+            tempData.Remove(SearchKey);
+            tempData.Remove(TypeKey);
+        }
+    }
+    
+}
---
+++ Week 16/SearchViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NaomiRetroCloset.Models
+{
+    public class SearchViewModel
+    {
+        public IEnumerable<ProductData> ProductData { get; set; }
+        [Required(ErrorMessage ="Please enter a search term.")]
+        public string SearchTerm { get; set; }
+        public String Type { get; set; }
+
+    }
+}
